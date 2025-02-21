8562b9d82f140436d00dd4d4d6a8f0c3c8883a88
jkguesswho <jkguesswho@gmail.com>
2025-02-20 23:56:10 -0800

    5: Modified main.py to create a compiled report of the book text. File main.py no longer prints the word count or the character count dictionary. File main.py now has a function that creates a multi-print report by converting the character count dictionary to a list of tuples, __.sort()'ing the list, and then printing only the ones where __.isalpha() == True. Also prints filepath and total word count as part of the added function.

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ def main():
     # fn creating a dict of each unique character in string, values of instance count of character
     char_count = get_num_chars(text)
     # print(text)    # print entire contents string to console
-    # print the value of word_count to console in a dymanic string
-    print(f"{word_count} words found in the document.")
-    # print the dictionary char_count
-    print("These are all the unique characters in the document and how many times each one occurs:")
-    print(char_count)
+    # print the value of word_count to console in a dymanic string (commented out)
+    # print(f"{word_count} words found in the document.")
+    # print the dictionary char_count (commented out)
+    # print("These are all unique characters in the document and how many times each one occurs:")
+    # print(char_count)
+    # fn creating a detailed report containing filepath, word count, and alphabetic occurances
+    gen_report(book_path, word_count, char_count)
 
 def read_book_text(path):
     # 'with' block closes files automatically when the block exits for any reason
@@ -43,5 +45,42 @@ def get_num_chars(text):
     # debug print statement to check for errors within function
     # print("test1", list(char_dict.items())[:5])
     return char_dict
+
+def gen_report(path, words, chars):
+    # establishing a whitelist of characters for later
+    alphabet = ("abcdefghijklmnopqrstuvwxyz")
+    # beginning report with the filepath
+    print(f"--- Begin report of {path} ---")
+    # printing the amount of words
+    print(f"{words} words found in the document")
+    # blank line for formatting
+    print()
+    # for loop iterating through the alphabet string
+    for letter in alphabet:
+        # printing iteration letter and value of corresponding key from input dictionary
+        print(f"The '{letter}' character was found {chars[letter]} times")
+    print("--- End report ---")
+
+"""
+# alternate method for creating report, with added frequency ordering!
+def gen_report(path, words, chars):
+    # converting dict to dict view object then to list for sorting as tuples
+    # a dict view object cannot be iterated on, but a list of tuples can be!
+    dict_view_obj = chars.items()
+    tuple_list = list(dict_view_obj)
+    tuple_list.sort(key = lambda item: item[1], reverse = True)
+    # beginning with filepath display
+    print(f"--- Begin report of {path} ---")
+    # printing the amount of words
+    print(f"{words} words in the document")
+    # blank line for formatting
+    print()
+    # iterating the list of tuples, checking each key (index 0) if it is an alphabetical character
+    for item in tuple_list:
+        # if the key is an alphabetical character, print the key and value
+        if item[0].isalpha() == True:
+            print(f"The '{item[0]}' character was found {item[1]} times")
+    print("--- End report ---")
+"""
 
 main()
